53087c406e9896a8ffa4c865792b5eeadcd9accc
Christine <christine_moldovan@yahoo.com>
2020-12-21 20:57:21 +0200

    Lab6

--- src/Lab5/Challenge5/NewException.java
+++
@@ -1,7 +0,0 @@
-package Lab5.Challenge5;
-
-public class NewException extends Exception{
-    public NewException(String message){
-        super(message);
-    }
-}
---
+++ src/Lab6/Challenge1/Main.java
@@ -0,0 +1,24 @@
+package Lab6.Challenge1;
+
+import java.util.*;
+
+public class Main {
+    public static void displayLeaders(List<Integer> list){
+        Collections.reverse(list);
+        Iterator<Integer> it = list.iterator();
+        Integer currentElement = list.get(0);
+        Integer max = list.get(0);
+        System.out.print(currentElement + " ");
+        while(it.hasNext()){
+            currentElement = it.next();
+            if(currentElement > max){
+                System.out.print(currentElement + " ");
+                max=currentElement;
+            }
+
+        }
+    }
+    public static void main(String[] args) {
+        displayLeaders(Arrays.asList(1,5,11,2,9,7,2,5,4,1));
+    }
+}
